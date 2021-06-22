bc91bba005d0a29e59cb0cb845227204c6ca9f51
Sehyun Chung <sehyun@berkeley.edu>
2021-06-22 15:51:08 -0400

    add more cards and watchlist mockup

--- src/components/VaultCard.tsx
+++ src/components/VaultCard.tsx
@@ -9,6 +9,17 @@ import {
 } from "@chakra-ui/react";
 import Image from "next/image";
 
+const GRADIENTS = {
+  ETH: {
+    start: "#c993ff",
+    end: "#415dff",
+  },
+  WBTC: {
+    start: "#F4B86B",
+    end: "#ff9d85",
+  },
+};
+
 type VaultCardProps = {
   name: string;
   platform: string;
@@ -31,11 +42,11 @@ const VaultCard = ({
       color="white"
       w="500px"
       rounded="10"
-      bgGradient="linear(to-r,#c993ff, #415dff )"
+      bgGradient={`linear(to-r, ${GRADIENTS[underlying]?.start}, ${GRADIENTS[underlying]?.end})`}
       position="relative"
     >
       <VStack spacing="4" align="left" px="6" py="3">
-        <Image src="/static/eth.svg" alt="eth" layout="fill" />
+        <Image src={`/static/${underlying}.svg`} alt="eth" layout="fill" />
         <div>
           <Tag mr="3">{platform}</Tag>
           <Tag>{underlying}</Tag>
@@ -50,7 +61,7 @@ const VaultCard = ({
             <Text>Current Desposits</Text>
             <Text>{`${current} ${underlying}`}</Text>
           </Flex>
-          <Progress value={80} />
+          <Progress value={(parseFloat(current) / parseFloat(max)) * 100} />
           <Flex align="center" justify="space-between">
             <Text>Max Capacity</Text>
             <Text>{`${max} ${underlying}`}</Text>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,51 +2,94 @@ import { Box } from "@chakra-ui/layout";
 import { Flex, Heading, HStack } from "@chakra-ui/react";
 import VaultCard from "@/components/VaultCard";
 
-const VAULTS = [
+const CURRENT_VAULTS = [
   {
     name: "T-ETH-C",
     platform: "Ribbon",
     underlying: "ETH",
-    apy: "6.98",
-    current: "12",
-    max: "120",
+    apy: "9.23",
+    current: "9999.99",
+    max: "10000",
   },
   {
-    name: "T-ETH-C",
+    name: "T-BTC-C",
     platform: "Ribbon",
-    underlying: "ETH",
-    apy: "6.98",
+    underlying: "WBTC",
+    apy: "2.31",
     current: "12",
     max: "120",
   },
-  {
-    name: "T-ETH-C",
-    platform: "Ribbon",
-    underlying: "ETH",
-    apy: "6.98",
-    current: "12",
-    max: "120",
-  },
+  // {
+  //   name: "Theta-Gang",
+  //   platform: "Fontis",
+  //   underlying: "ETH",
+  //   apy: "6.78",
+  //   current: "22",
+  //   max: "1000",
+  // },
+  // {
+  //   name: "Theta-Gang",
+  //   platform: "Fontis",
+  //   underlying: "ETH",
+  //   apy: "6.78",
+  //   current: "22",
+  //   max: "1000",
+  // },
 ];
 
+const WATCH_VAULTS = [
+  {
+    name: "Theta-Gang",
+    platform: "Fontis",
+    underlying: "ETH",
+    apy: "6.78",
+    current: "22",
+    max: "1000",
+  },
+  {
+    name: "Theta-Gang",
+    platform: "Fontis",
+    underlying: "ETH",
+    apy: "6.78",
+    current: "22",
+    max: "1000",
+  },
+];
 const Home = () => {
   return (
     <Box mb={8} w="full">
       <Heading>Your Vaults</Heading>
-      <HStack align="center" justify="space-evenly" p="6" spacing="6">
-        {VAULTS.map(({ name, platform, underlying, apy, current, max }) => (
-          <VaultCard
-            key={name}
-            name={name}
-            platform={platform}
-            underlying={underlying}
-            apy={apy}
-            current={current}
-            max={max}
-          />
-        ))}
+      <HStack align="center" p="6" spacing="6">
+        {CURRENT_VAULTS.map(
+          ({ name, platform, underlying, apy, current, max }) => (
+            <VaultCard
+              key={name}
+              name={name}
+              platform={platform}
+              underlying={underlying}
+              apy={apy}
+              current={current}
+              max={max}
+            />
+          )
+        )}
       </HStack>
-      {/* <Heading>Watchlist</Heading> */}
+      <Heading>Watchlist</Heading>
+      <HStack align="center" p="6" spacing="6">
+        {WATCH_VAULTS.map(
+          ({ name, platform, underlying, apy, current, max }) => (
+            <VaultCard
+              key={name}
+              name={name}
+              platform={platform}
+              underlying={underlying}
+              apy={apy}
+              current={current}
+              max={max}
+            />
+          )
+        )}
+      </HStack>
     </Box>
   );
 };
