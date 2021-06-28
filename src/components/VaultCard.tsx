830e52b9ceda7f77cd2a3b9aeff4f6b09d229bdc
Sehyun Chung <sehyun@berkeley.edu>
2021-06-27 21:50:43 -0400

    remove link from vaultcard

--- src/components/VaultCard.tsx
+++ src/components/VaultCard.tsx
@@ -1,16 +1,16 @@
+import AccessibleLink from "@/components/AccessibleLink";
 import {
-  Image,
-  Grid,
+  useDisclosure,
   Box,
   Flex,
+  Grid,
   Heading,
+  Image,
   Progress,
   Tag,
   Text,
-  VStack,
+  Slide,
 } from "@chakra-ui/react";
-// import Image from "next/image";
-import AccessibleLink from "@/components/AccessibleLink";
 
 const COLORS = {
   ETH: {
@@ -42,90 +42,84 @@ const VaultCard = ({
   current,
   max,
 }: VaultCardProps) => {
+  const { isOpen, onToggle } = useDisclosure();
   return (
-    <AccessibleLink href={`/${platform.toLowerCase()}/${name.toLowerCase()}`}>
-      <Box
-        minW="30rem"
-        borderRadius="lg"
-        boxShadow="surface"
-        overflow="hidden"
-        cursor="pointer"
-        transition={`
+    // <AccessibleLink href={`/${platform.toLowerCase()}/${name.toLowerCase()}`}>
+    <Box
+      onClick={onToggle}
+      minW="30rem"
+      borderRadius="lg"
+      boxShadow="surface"
+      overflow="hidden"
+      cursor="pointer"
+      transition={`
         transform 0.5s cubic-bezier(0, 0.28, 0.45, 0.95),
         box-shadow 0.5s cubic-bezier(0, 0.28, 0.45, 0.95)
-        
         `}
-        _hover={{
-          outline: "none",
-          transform: "scale(1.02)",
-          boxShadow: "surface_hovered",
-        }}
-        _focus={{
-          outline: "none",
-          transform: "scale(1.02)",
-          boxShadow: "surface_hovered",
-        }}
+      _hover={{
+        outline: "none",
+        transform: "scale(1.02)",
+        boxShadow: "surface_hovered",
+      }}
+    >
+      <Grid
+        gap={0}
+        p="6"
+        templateColumns="1fr"
+        flex="1"
+        color="white"
+        position="relative"
+        bgGradient={`linear(to-r, ${COLORS[underlying]?.start}, ${COLORS[underlying]?.end})`}
       >
-        <Grid
-          gap={0}
-          p="6"
-          templateColumns="1fr"
-          flex="1"
-          color="white"
-          position="relative"
-          bgGradient={`linear(to-r, ${COLORS[underlying]?.start}, ${COLORS[underlying]?.end})`}
-        >
-          <Image
-            // zIndex="1"
-            src={`/static/${underlying}.svg`}
-            alt="logo"
-            minH="100%"
-            minW="100%"
-            transformOrigin="50% 50%"
-            maxWidth="150%"
-            position="absolute"
-            userSelect="none"
-            right="0"
-            transition={`
+        <Image
+          src={`/static/${underlying}.svg`}
+          alt="logo"
+          minH="100%"
+          minW="100%"
+          transformOrigin="50% 50%"
+          maxWidth="150%"
+          position="absolute"
+          userSelect="none"
+          right="0"
+          transition={`
             transform 0.5s cubic-bezier(0, 0.28, 0.45, 0.95)
           `}
-            _hover={{
-              transition: `
+          _hover={{
+            transition: `
               transform 0.5s cubic-bezier(0, 0.28, 0.45, 0.95),
               box-shadow 0.5s cubic-bezier(0, 0.28, 0.45, 0.95)
               `,
-              transform: "scale(1.2)",
-            }}
-          />
-          <Box pointerEvents="none" zIndex="1">
-            <Box mb="1">
-              <Tag variant="solid" colorScheme={COLORS[underlying]?.tag} mr="3">
-                {platform}
-              </Tag>
-              <Tag variant="solid" colorScheme={COLORS[underlying]?.tag}>
-                {underlying}
-              </Tag>
-            </Box>
-            <Heading color="white">{"ETH-A"}</Heading>
-            <div>
-              <Text>Projected APY</Text>
-              <Heading fontFamily="Epilogue">{`${apy}%`}</Heading>
-            </div>
-            <div>
-              <Flex align="center" justify="space-between">
-                <Text>Current Deposits</Text>
-                <Text>{`${current} ${underlying}`}</Text>
-              </Flex>
-              <Progress value={(parseFloat(current) / parseFloat(max)) * 100} />
-              <Flex align="center" justify="space-between">
-                <Text>Max Capacity</Text>
-                <Text>{`${max} ${underlying}`}</Text>
-              </Flex>
-            </div>
+            transform: "scale(1.2)",
+          }}
+        />
+        <Box pointerEvents="none" zIndex="1">
+          <Box mb="1">
+            <Tag variant="solid" colorScheme={COLORS[underlying]?.tag} mr="3">
+              {platform}
+            </Tag>
+            <Tag variant="solid" colorScheme={COLORS[underlying]?.tag}>
+              {underlying}
+            </Tag>
           </Box>
-        </Grid>
-      </Box>
-    </AccessibleLink>
+          <Heading color="white">{name}</Heading>
+          <div>
+            <Text>Projected APY</Text>
+            <Heading fontFamily="Epilogue">{`${apy}%`}</Heading>
+          </div>
+          <div>
+            <Flex align="center" justify="space-between">
+              <Text>Current Deposits</Text>
+              <Text>{`${current} ${underlying}`}</Text>
+            </Flex>
+            <Progress value={(parseFloat(current) / parseFloat(max)) * 100} />
+            <Flex align="center" justify="space-between">
+              <Text>Max Capacity</Text>
+              <Text>{`${max} ${underlying}`}</Text>
+            </Flex>
+          </div>
+        </Box>
+      </Grid>
+    </Box>
   );
 };
 
