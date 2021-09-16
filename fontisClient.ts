17e64f7b8d7978fb902b889a8238f4c78357eaf5
Sehyun Chung <sehyun@berkeley.edu>
2021-09-15 22:17:25 -0700

    file cleanup

--- fontisClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const fontisClient = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/fontus-god/fontis",
-  cache: new InMemoryCache(),
-});
-
-export default fontisClient;
--- ribbonClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const ribbonClient = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/kenchangh/ribbon-finance",
-  cache: new InMemoryCache(),
-});
-
-export default ribbonClient;
